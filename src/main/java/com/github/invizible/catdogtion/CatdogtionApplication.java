49e5e4963ff53372cde2e9a7df31fa225db278d1
Korobko Alex <korobkoalexander5901@gmail.com>
2016-10-08 18:55:25 +0300

    Added spring boot

---
+++ src/main/java/com/github/invizible/catdogtion/CatdogtionApplication.java
@@ -0,0 +1,12 @@
+package com.github.invizible.catdogtion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatdogtionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatdogtionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/invizible/catdogtion/CatdogtionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.invizible.catdogtion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CatdogtionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
