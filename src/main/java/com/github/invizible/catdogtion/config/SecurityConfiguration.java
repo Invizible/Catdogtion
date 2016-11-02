114d15f360fbfb508df5b01e617ee6b40e6fc6ac
Alex Korobko <akorobko@connecture.com>
2016-11-02 15:23:32 +0200

    Added spring security config extension point

--- src/main/java/com/github/invizible/catdogtion/config/SecurityConfiguration.java
+++ src/main/java/com/github/invizible/catdogtion/config/SecurityConfiguration.java
@@ -1,7 +1,11 @@
 package com.github.invizible.catdogtion.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,5 +15,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception
+  {
+    super.configure(http);
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception
+  {
+    super.configure(auth);
+  }
 }
