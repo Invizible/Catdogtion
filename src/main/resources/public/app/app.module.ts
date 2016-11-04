5d04b122ffaf6e6243031848c34aeddc2879c0e6
Korobko Alex <korobkoalexander5901@gmail.com>
2016-11-04 04:02:41 +0200

    Added navbar component

--- src/main/resources/public/app/app.module.ts
+++ src/main/resources/public/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {CollapseDirective} from 'ng2-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CollapseDirective
   ],
   imports: [
     BrowserModule,
