1989609a29c41ae07af32b0d732611dbb5679060
Alex Korobko <akorobko@connecture.com>
2016-11-11 18:26:14 +0200

    updating lots

--- src/main/resources/public/app/lot-modal/lot-modal.component.ts
+++ src/main/resources/public/app/lot-modal/lot-modal.component.ts
@@ -29,7 +29,7 @@ export class LotModalComponent implements OnInit {
   lot: Lot = new Lot();
 
   @Output()
-  addLot: EventEmitter<Lot> = new EventEmitter();
+  addLot: EventEmitter<Lot> = new EventEmitter<Lot>();
 
   constructor(private cookieService: CookieService,
               private imageService: ImageService,
